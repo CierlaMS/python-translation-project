cc344ab786c8ea4c92c992bf0f719d99c1f60e92
Cierla <czm0119@auburn.edu>
2020-04-06 16:56:55 -0500

    create translate_orf.py and add parser

---
+++ translate_orf.py
@@ -0,0 +1,17 @@
+#! /usr/bin/env python3
+
+#script to translate the first ORF found in a nucleotide sequence
+
+import sys
+import find_orf
+import translate
+import argparse
+
+p = argparse.ArgumentParser(
+    formatter_class = argparse.ArgumentDefaultsHelpFormatter)
+p.add_argument("
+
+p.add_argument(
+
+p.add_argument(
+
