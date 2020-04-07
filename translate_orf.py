3f6d863f8aa70744c0765d9265f6b65f92771a35
Cierla <czm0119@auburn.edu>
2020-04-06 19:49:17 -0500

    added more parsing info

--- translate_orf.py
+++ translate_orf.py
@@ -7,11 +7,17 @@ import find_orf
 import translate
 import argparse
 
-p = argparse.ArgumentParser(
-    formatter_class = argparse.ArgumentDefaultsHelpFormatter)
-p.add_argument("
+p = argparse.ArgumentParser(descritption=usage)
+p.add_argument('Path'
+        metavar='path',
+        type=str
+        help='the path to list'
 
 p.add_argument(
 
 p.add_argument(
 
+args = p.parse_args()
+
+print(args.input)
+genetic_code = {'GUC': 'V', 'ACC': 'T', 'GUA': 'V', 'GUG': 'V', 'ACU': 'T', 'AAC': 'N', 'CCU': 'P', 'UGG': 'W', 'AGC': 'S', 'AUC': 'I', 'CAU': 'H', 'AAU': 'N', 'AGU': 'S', 'GUU': 'V', 'CAC': 'H', 'ACG': 'T', 'CCG': 'P', 'CCA': 'P', 'ACA': 'T', 'CCC': 'P', 'UGU': 'C', 'GGU': 'G', 'UCU': 'S', 'GCG': 'A', 'UGC': 'C', 'CAG': 'Q', 'GAU': 'D', 'UAU': 'Y', 'CGG': 'R', 'UCG': 'S', 'AGG': 'R', 'GGG': 'G', 'UCC': 'S', 'UCA': 'S', 'UAA': '*', 'GGA': 'G', 'UAC': 'Y', 'GAC': 'D', 'UAG': '*', 'AUA': 'I', 'GCA': 'A', 'CUU': 'L', 'GGC': 'G', 'AUG': 'M', 'CUG': 'L', 'GAG': 'E', 'CUC': 'L', 'AGA': 'R', 'CUA': 'L', 'GCC': 'A', 'AAA': 'K', 'AAG': 'K', 'CAA': 'Q', 'UUU': 'F', 'CGU': 'R', 'CGC': 'R', 'CGA': 'R', 'GCU': 'A', 'GAA': 'E', 'AUU': 'I', 'UUG': 'L', 'UUA': 'L', 'UGA': '*', 'UUC': 'F'}
