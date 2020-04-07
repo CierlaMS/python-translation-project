fa66bb25fe9a84917f25440585f96413c1b8b223
CierlaMS <60618975+CierlaMS@users.noreply.github.com>
2020-04-06 20:26:05 -0500

    updated code

--- translate_orf.py
+++ translate_orf.py
@@ -6,46 +6,36 @@ def main():
     import find_orf
     import translate
     import argparse
-def main():
-    import argparse
 
     # Create a command-line parser object
-    parser = argparse.ArgumentParser(
+    p = argparse.ArgumentParser(
             formatter_class = argparse.ArgumentDefaultsHelpFormatter)
 
-<<<<<<< HEAD
     # Tell the parser what command-line arguments this script can receive
-    parser.add_argument('sequence',
+    p.add_argument('sequence',
             metavar = 'SEQUENCE',
             type = str,
             help = ('The sequence to search for an open-reading frame. '
                     'If the path flag (\'-p\'/\'--path\') is specified, '
                     'then this should be a path to a file containing the '
                     'sequence to be searched.'))
-    parser.add_argument('-p', '--path',
+    p.add_argument('-p', '--path',
             action = 'store_true',
             help = ('The sequence argument should be treated as a path to a '
                     'containing the sequence to be searched.'))
-    parser.add_argument('-s', '--start-codons',
+    p.add_argument('-s', '--start-codons',
             type = str,
             nargs = '+', # one or more arguments
             default = ['AUG'],
             help = ('One or more possible start codons.'))
-    parser.add_argument('-x', '--stop-codons',
+    p.add_argument('-x', '--stop-codons',
             type = str,
             nargs = '+', # one or more arguments
             default = ['UAA', 'UAG', 'UGA'],
             help = ('One or more possible stop codons.'))
-=======
-p = argparse.ArgumentParser(description=usage)
-p.add_argument('Path'
-        metavar='path',
-        type=str
-        help='the path to list'
->>>>>>> 61c7a46c812d3d652eb32691b2ef553477f8c7bc
-
+    
     # Parse the command-line arguments into a 'dict'-like container
-    args = parser.parse_args()
+    args = p.parse_args()
 
     # Check to see if the path option was set to True by the caller. If so, parse
     # the sequence from the path
@@ -57,23 +47,12 @@ p.add_argument('Path'
     orf = find_first_orf(sequence = sequence,
             start_codons = args.start_codons,
             stop_codons = args.stop_codons)
+    genetic_code = {'GUC': 'V', 'ACC': 'T', 'GUA': 'V', 'GUG': 'V', 'ACU': 'T', 'AAC': 'N', 'CCU': 'P', 'UGG': 'W', 'AGC': 'S', 'AUC': 'I', 'CAU': 'H', 'AAU': 'N', 'AGU': 'S', 'GUU': 'V', 'CAC': 'H', 'ACG': 'T', 'CCG': 'P', 'CCA': 'P', 'ACA': 'T', 'CCC': 'P', 'UGU': 'C', 'GGU': 'G', 'UCU': 'S', 'GCG': 'A', 'UGC': 'C', 'CAG': 'Q', 'GAU': 'D', 'UAU': 'Y', 'CGG': 'R', 'UCG': 'S', 'AGG': 'R', 'GGG': 'G', 'UCC': 'S', 'UCA': 'S', 'UAA': '*', 'GGA': 'G', 'UAC': 'Y', 'GAC': 'D', 'UAG': '*', 'AUA': 'I', 'GCA': 'A', 'CUU': 'L', 'GGC': 'G', 'AUG': 'M', 'CUG': 'L', 'GAG': 'E', 'CUC': 'L', 'AGA': 'R', 'CUA': 'L', 'GCC': 'A', 'AAA': 'K', 'AAG': 'K', 'CAA': 'Q', 'UUU': 'F', 'CGU': 'R', 'CGC': 'R', 'CGA': 'R', 'GCU': 'A', 'GAA': 'E', 'AUU': 'I', 'UUG': 'L', 'UUA': 'L', 'UGA': '*', 'UUC': 'F'}
+    run_translate_orf = translate.translate_sequence(orf,genetic_code)
     sys.stdout.write('{}\n'.format(orf))
 
 
 if __name__ == '__main__':
     main()
-    p = argparse.ArgumentParser(descritption=usage)
-    p.add_argument('Path'
-            metavar='path',
-            type=str
-            help='the path to list'
 
-    p.add_argument(
-
-    p.add_argument(
-
-    args = p.parse_args()
-
-    print(args.input)
-
-    genetic_code = {'GUC': 'V', 'ACC': 'T', 'GUA': 'V', 'GUG': 'V', 'ACU': 'T', 'AAC': 'N', 'CCU': 'P', 'UGG': 'W', 'AGC': 'S', 'AUC': 'I', 'CAU': 'H', 'AAU': 'N', 'AGU': 'S', 'GUU': 'V', 'CAC': 'H', 'ACG': 'T', 'CCG': 'P', 'CCA': 'P', 'ACA': 'T', 'CCC': 'P', 'UGU': 'C', 'GGU': 'G', 'UCU': 'S', 'GCG': 'A', 'UGC': 'C', 'CAG': 'Q', 'GAU': 'D', 'UAU': 'Y', 'CGG': 'R', 'UCG': 'S', 'AGG': 'R', 'GGG': 'G', 'UCC': 'S', 'UCA': 'S', 'UAA': '*', 'GGA': 'G', 'UAC': 'Y', 'GAC': 'D', 'UAG': '*', 'AUA': 'I', 'GCA': 'A', 'CUU': 'L', 'GGC': 'G', 'AUG': 'M', 'CUG': 'L', 'GAG': 'E', 'CUC': 'L', 'AGA': 'R', 'CUA': 'L', 'GCC': 'A', 'AAA': 'K', 'AAG': 'K', 'CAA': 'Q', 'UUU': 'F', 'CGU': 'R', 'CGC': 'R', 'CGA': 'R', 'GCU': 'A', 'GAA': 'E', 'AUU': 'I', 'UUG': 'L', 'UUA': 'L', 'UGA': '*', 'UUC': 'F'}
+   
